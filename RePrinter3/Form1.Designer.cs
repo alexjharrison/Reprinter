53c493d6ffa2afd01f2670ac2c305a1c792c2700
alexmon32 <alexmon32@live.com>
2015-10-07 10:13:22 -0400

    commit1

---
+++ RePrinter3/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace RePrinter3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Material Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Batch Number";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(319, 116);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(84, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Topex Label";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(319, 243);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Box Label";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(319, 220);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(94, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Barcode Label";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(68, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(68, 218);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 80);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Print Labels";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(319, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Disc Numbers";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(319, 170);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(33, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "to";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(381, 169);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(33, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(319, 299);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(33, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(319, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Quantity";
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(296, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(179, 2);
+            this.label6.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Location = new System.Drawing.Point(296, 333);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(179, 2);
+            this.label7.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(28, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(153, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(287, 52);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Label Reprinter";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(228, 406);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(247, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Copyright 2015® Harrison Inc ™ LLC DMD Esq Ltd";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(418, 117);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 17);
+            this.checkBox1.TabIndex = 20;
+            this.checkBox1.Text = "Retain";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 428);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IVMI Label Reprinter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ RePrinter3/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace RePrinter3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Select();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string materialNum = textBox1.Text;
+            string batchNum = textBox2.Text.ToUpper();
+            int quantity = 0;
+            int firstDisc; int lastDisc; string materialCode = ""; string shade = ""; string diameter = ""; string thickness = ""; string brand = ""; string shadeCopy = ""; string barcodeIdentifier = "";
+            string specListLocation = "G:/LabX_Export/LabsQ_LabX_Integration/";
+            File.Copy(specListLocation + "Wieland_Ardent_List_Separate.csv", specListLocation + "Wieland_Ardent_List_Tempcopy.csv",true);
+            var column1 = new List<string>();
+            var column2 = new List<string>();
+            var column3 = new List<string>();
+            var column4 = new List<string>();
+            var column5 = new List<string>();
+            var column6 = new List<string>();
+            var column7 = new List<string>();
+            var column8 = new List<string>();
+            var column9 = new List<string>();
+            var column10 = new List<string>();
+            var column11 = new List<string>();
+            var column12 = new List<string>();
+            using (var rd = new StreamReader(specListLocation + "Wieland_Ardent_List_Tempcopy.csv"))
+            {
+                while (!rd.EndOfStream)
+                {
+                    string theNextLine = rd.ReadLine();
+                    if (theNextLine == "") break;
+                    var splits = theNextLine.Split(',');
+                    column1.Add(splits[0]);
+                    column2.Add(splits[1]);
+                    column3.Add(splits[2]);
+                    column4.Add(splits[3]);
+                    column5.Add(splits[4]);
+                    column6.Add(splits[5]);
+                    column7.Add(splits[6]);
+                    column8.Add(splits[2]);
+                    column9.Add(splits[3]);
+                    column10.Add(splits[4]);
+                    column11.Add(splits[5]);
+                    column12.Add(splits[6]);
+                }
+            }
+            File.Delete(specListLocation + "Wieland_Ardent_List_Tempcopy.csv");
+            for (int i = 0; i < column1.Count; i++)
+            {
+                if (column1[i] == materialNum)
+                {
+                    materialCode = column2[i];
+                    shade = column6[i];
+                    shadeCopy = shade;
+                    diameter = column12[i];
+                    thickness = column3[i];
+                    brand = column5[i];
+                    barcodeIdentifier = column4[i].Replace('_',' ');
+                    if (shadeCopy == "Sun-Chroma") shadeCopy = shadeCopy.Replace("-", " ");
+                    if (Char.IsDigit(shadeCopy[1])) shadeCopy = shadeCopy.Insert(1, " ");
+                    else if (Char.IsDigit(shadeCopy[2])) shadeCopy = shadeCopy.Insert(2, " ");
+                    if (materialCode == "ZTR" || shadeCopy == "Sun" || shadeCopy == "Sun Chroma") shadeCopy = shadeCopy.ToLower();
+                }
+            }
+            
+            if (shade.Equals(""))
+            { MessageBox.Show("Material Not Found in Wieland_Ardent_List", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return; }
+            string labelInfoLocation = "G:/Equipment/GiS-Topex RFID Printer/Generated RFID Files/" + shade + "/" + batchNum + "/";
+
+
+            //Topex Printer
+            if(radioButton1.Checked==true)  
+            {
+                if(textBox3.Text == "")
+                {
+                    MessageBox.Show("Please enter range of discs to print", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); 
+                    return;
+                }
+                if (textBox4.Text == "") textBox4.Text = textBox3.Text;
+                try
+                { firstDisc = int.Parse(textBox3.Text); lastDisc = int.Parse(textBox4.Text); }
+                catch
+                { MessageBox.Show("Disc range not numbers", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return; }
+                if (firstDisc > lastDisc)
+                { MessageBox.Show("First disc larger than last disc", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return; }
+                
+                //generate new rfid file
+                string[] lines;
+                try { lines = File.ReadAllLines(labelInfoLocation + batchNum + "_rfid_complete.csv"); }
+                catch 
+                { 
+                    MessageBox.Show(labelInfoLocation + batchNum + "_rfid_complete.csv\nhas not been measured.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                                
+                File.WriteAllText(labelInfoLocation + batchNum + "_rfid_temp.csv",lines[0]+"\n");
+                if (lastDisc > lines.Length-1)
+                {
+                    MessageBox.Show("Higher number exceeds batch size", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                if(firstDisc<1)
+                {
+                    MessageBox.Show("No discs less than zero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                for (int i = 0; i <= lastDisc - firstDisc; i++)
+                {
+                    File.AppendAllText(labelInfoLocation + batchNum + "_rfid_temp.csv", lines[firstDisc + i] + "\n");
+                }
+
+
+                ProcessStartInfo theDruck = new ProcessStartInfo();
+                theDruck.CreateNoWindow = true;
+                theDruck.UseShellExecute = false;
+                theDruck.WindowStyle = ProcessWindowStyle.Hidden;
+                theDruck.FileName = @"C:\Program Files (x86)\Wieland RFID PrinterStation\DruckerStation.exe";
+
+                //print FC labels
+                if(materialCode == "ZFC" && checkBox1.Checked == false)
+                    theDruck.Arguments = " /P \"G:\\Topex_Printer\\Zirlux Label Templates\\Zirlux FC2\\Zirlux FC2 With Ring.txt\" /RFID Off  /B \"" + labelInfoLocation + batchNum + "_rfid_temp.csv\"  /start /hidden";
+                //print FC retain labels
+                else if (materialCode == "ZFC" && checkBox1.Checked == true)
+                    theDruck.Arguments = " /P \"G:\\Topex_Printer\\Zirlux Label Templates\\Zirlux FC2\\Zirlux FC2 With Ring_Retain.txt\" /RFID Off  /B \"" + labelInfoLocation + batchNum + "_rfid_temp.csv\"  /start /hidden";
+                //print wieland labels
+                else if (checkBox1.Checked == false)
+                    theDruck.Arguments = " /P \"G:\\Topex_Printer\\Wieland_CE0123.txt\" /RFID On  /B \"" + labelInfoLocation + batchNum + "_rfid_temp.csv\"  /start /hidden";
+                //print wieland retains labels
+                else
+                    theDruck.Arguments = " /P \"G:\\Topex_Printer\\Wieland Label Templates\\Wieland_CE0123_Retain.txt\" /RFID On  /B \"" + labelInfoLocation + batchNum + "_rfid_temp.csv\"  /start /hidden";
+
+                //run druckerstation
+                try
+                {
+                    using (Process exeProcess = Process.Start(theDruck))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Druckerstation.exe not found or misconfigured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+
+                //delete temporary file
+                File.Delete(labelInfoLocation + batchNum + "_rfid_temp.csv");
+            }
+            
+
+
+            //Box & Barcode Label
+            else if (radioButton2.Checked==true || radioButton3.Checked == true)
+            {
+                if (textBox5.Text=="")
+                {
+                    MessageBox.Show("Please enter a quantity", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return;
+                }
+                try
+                { quantity = int.Parse(textBox5.Text); }
+                catch
+                { MessageBox.Show("Quantity not a number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return; }
+                if (quantity < 1)
+                { MessageBox.Show("Please enter a valid quantity", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return; }
+
+                string labelName = "";
+                if (radioButton2.Checked == true)
+                    labelName = "Box Output_" + batchNum + ".cmd";
+                else if (radioButton3.Checked == true)
+                    labelName = "Barcode Output_" + batchNum + ".cmd";
+                string lines = "";
+                string cmdOutputLocation = @"G:\Equipment\GiS-Topex RFID Printer\Datamax Template Files\";
+                try { lines = File.ReadAllText(labelInfoLocation + labelName); }
+                catch
+                {
+                    DialogResult question = MessageBox.Show("Warning!\n" + labelInfoLocation + labelName + "\nhas not been measured.\nPrint anyway?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation); ;
+                    if (question == DialogResult.No) return;
+                    if (question == DialogResult.Yes)
+                    {
+                        if (radioButton2.Checked == true)
+                            lines = "LABELNAME = \"G:\\Equipment\\GiS-Topex RFID Printer\\Datamax Template Files\\Zenostar Box Label Template.lab\"" + Environment.NewLine +
+                                    "PRINTER= \"Copy of Datamax-O'Neil I-4606e Mark II,USB004\"" + Environment.NewLine +
+                                    "Thickness = \"" + thickness + "\"" + Environment.NewLine +
+                                    "Material Number = \"" + materialNum + "\"" + Environment.NewLine +
+                                    "Lot Number = \"" + batchNum + "\"" + Environment.NewLine +
+                                    "Material Description = \"" + shadeCopy + "\"" + Environment.NewLine +
+                                    "LABELQUANTITY = \"" + quantity + "\"";
+                        else if (radioButton3.Checked == true)
+                            lines =
+                                "LABELNAME = \"G:\\Equipment\\GiS-Topex RFID Printer\\Datamax Template Files\\Zenostar Barcode Label Template.lab\"" + Environment.NewLine +
+                                "PRINTER = \"Datamax-O'Neil I-4606e Mark II,USB003\"" + Environment.NewLine +
+                                "Material Number = \"" + materialNum + "\"" + Environment.NewLine +
+                                "Lot Number = \"" + batchNum + "\"" + Environment.NewLine +
+                                "Piece Count = 1 pc.\n" + Environment.NewLine +
+                                "Barcode Label Identifier = \"" + barcodeIdentifier + " \"" + Environment.NewLine +
+                                "LABELQUANTITY = \"" + quantity + "\"";
+
+                    }
+                }
+                
+                
+                lines = lines.Replace("LABELQUANTITY = \"2\"", "LABELQUANTITY = \""+quantity+"\"");
+                File.WriteAllText(cmdOutputLocation+"Label Output/Reprint Output.cmd",lines);
+
+                //run CS.exe
+                ProcessStartInfo csStart = new ProcessStartInfo();
+                csStart.CreateNoWindow = true;
+                csStart.UseShellExecute = false;
+                csStart.WindowStyle = ProcessWindowStyle.Hidden;
+                csStart.FileName = @"C:\Program Files (x86)\Teklynx\CODESOFT 2014\CS.exe";
+                csStart.Arguments = " /CMD " + cmdOutputLocation + "Label Output";
+
+                try
+                {
+                    using (Process exeProcess = Process.Start(csStart))
+                        exeProcess.WaitForExit(1);
+                }
+                catch
+                {
+                    File.Delete(cmdOutputLocation + "Label Output/Reprint Output.cmd");
+                    MessageBox.Show("CS.exe not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                Thread.Sleep(5000);
+                MessageBox.Show(new Form() { TopMost = true },"You can close CodeSoft when labels finish printing", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            else
+            { MessageBox.Show("No Label Selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return; }
+            
+        }
+
+
+
+
+    }
+}
